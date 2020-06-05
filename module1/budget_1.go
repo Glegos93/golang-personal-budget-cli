83b2616ce383210376486e3bf0e8beb764f6552d
Carlos Gallegos <Carlos_E_Gallegos@homedepot.com>
2020-06-05 16:01:23 -0500

    Completed module

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	max   float32
+	Items []Item
+}
 
 // Item stores item information
+type Item struct {
+	Description string
+	price       float32
+}
